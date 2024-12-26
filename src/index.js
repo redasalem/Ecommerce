8f76fe6c9d97e3b539830f46f27a4f2e8355d15e
Your Name <you@example.com>
2024-12-26 18:53:31 +0200

    most sold product

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import'bootstrap/dist/css/bootstrap.min.css';
 import'./css/style.css';
 import'@fortawesome/fontawesome-free/js/all.min';
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+document.querySelectorAll('.add-to-card-btn').forEach(function (el) {
+    el.onclick = function () {
+       window.alert("اضيف المنتج الى عربة الشراء");
+    }
+ });
