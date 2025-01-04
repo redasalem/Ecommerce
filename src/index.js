65d994f498636fe1cedb979d1289e5f28d496ea8
Your Name <you@example.com>
2025-01-04 17:24:09 +0200

    product page done

--- src/index.js
+++ src/index.js
@@ -5,11 +5,33 @@ import'@fortawesome/fontawesome-free/js/all.min';
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
-document.querySelectorAll('.add-to-card-btn').forEach(function (el) {
+document.querySelectorAll('.add-to-card-btn').forEach( (el)=> 
     el.onclick = function () {
        window.alert("اضيف المنتج الى عربة الشراء");
     }
- });
+ );
+// ---------------------------------------------------------------------------------
+document.querySelectorAll('.size-option input[type="radio"]').forEach(function(ele){
+   ele.addEventListener('change',function(){
+    document.querySelectorAll('.size-option').forEach(function(i){
+       i.classList.remove("active")
+    })
+    ele.parentNode.parentNode.classList.add("active")
+   })
+  });
+                     //   ---------------------
+
+
+  document.querySelectorAll('.color-option input[type="radio"]').forEach(function(ele){
+   ele.addEventListener('change',function(){
+    document.querySelectorAll('.color-option').forEach(function(i){
+       i.classList.remove("active")
+    })
+    ele.parentNode.parentNode.classList.add("active")
+   })
+  });
+                    //    ---------------------------------------
+
 
  const currentYear = new Date().getFullYear();
  document.getElementById('copyright').textContent =`جميع الحقوق محفوظة لمتجر لسنة ${currentYear}`;
